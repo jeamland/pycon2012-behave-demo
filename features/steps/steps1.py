c84e113bf89d5e91a7ef29cb9d3cc925be194c6c
Benno Rice <benno@jeamland.net>
2012-08-23 11:20:21 +1000

    Make all the initial steps pass.

--- features/steps/steps1.py
+++ features/steps/steps1.py
@@ -8,3 +8,10 @@ SERVER_URL = 'http://127.0.0.1:8000/'
 @when(u'I fetch the meetup page')
 def impl(context):
     context.page = requests.get(SERVER_URL).content
+
+@then(u'I should see the meetup form')
+def impl(context):
+    soup = BeautifulSoup(context.page)
+    form = soup.find('form')
+    assert form is not None
+    assert form['action'] == '/add'
