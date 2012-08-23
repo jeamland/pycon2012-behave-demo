57722e7b04a623d011e491621c5c67ddc8cc8f7c
Benno Rice <benno@jeamland.net>
2012-08-23 11:26:51 +1000

    Implement the rest of the steps for this feature/scenario.

--- features/steps/steps2.py
+++ features/steps/steps2.py
@@ -20,3 +20,53 @@ def impl(context, flight):
             return
 
     assert False
+
+@when(u'I select "{day}" as my arrival day')
+def impl(context, day):
+    context.day = day.lower().title()
+
+    control = context.browser.form.find_control('day')
+    items = control.get_items()
+    for item in control.get_items():
+        if item.attrs['label'].lower() == day.lower():
+            control.value = [item.attrs['value']]
+            return
+
+    assert False
+
+@when(u'I enter "{name}" as my name')
+def impl(context, name):
+    context.name = name
+    context.browser.form.find_control('name').value = name
+
+@when(u'I click "Add me!"')
+def impl(context):
+    context.browser.submit()
+
+@then(u'my flight should appear in the flights table')
+def impl(context):
+    context.browser.response().seek(0)
+    soup = BeautifulSoup(context.browser.response().read())
+
+    table = soup.find('table')
+    flight_row = table.find('td', text=lambda t: context.flight in t).parent
+    assert flight_row is not None
+
+    for row in flight_row.previous_siblings:
+        if type(row) is not Tag:
+            continue
+        if row.find('td', text=context.day):
+            return
+        elif row.find('td', colspan='6'):
+            break
+    assert False
+
+@then(u'my name should appear next to my flight')
+def impl(context):
+    context.browser.response().seek(0)
+    soup = BeautifulSoup(context.browser.response().read())
+
+    name = soup.find(text=lambda t: context.name in t)
+    assert name
+    row = name.find_parent('tr')
+    assert row.find(text=lambda t: context.flight in t)
